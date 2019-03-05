d71572b4899ce7347bb4391ffb45a45bb2af8dff
DanManN <dnahimov@gmail.com>
2019-03-04 20:10:34 -0500

    cclayer matrix

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 GRAPH := simplegraph
 LAYER := 0
 
-PRODUCT := buffkcore wave connectedcomponents
+PRODUCT := buffkcore wave connectedcomponents cc-layers-mat
 
 CXX := g++
 LINKER := g++
@@ -68,6 +68,14 @@ ccs:
 	FILENAME=$$(echo $(GRAPH)/$(GRAPH)_layers/*-$$(python -c "import sys, json; print(json.load(sys.stdin)['$(LAYER)']['file_suffix'])" < $(GRAPH)/$(GRAPH)-layer-info.json).csv); \
 	[ -f "$$FILENAME" ] || FILENAME=$(GRAPH)/$(GRAPH); \
 	./connectedcomponents "$$FILENAME" $(LAYER) $(GRAPH)/$(GRAPH)_layers
+
+.PHONY: ccs
+
+cc-layers:
+	for FILE in $$(ls $(GRAPH)/$(GRAPH)_layers | grep .csv); do \
+		echo $$FILE; \
+		./cc-layers-mat $(GRAPH)/$(GRAPH)_layers/"$$FILE" $(GRAPH)/$(GRAPH).cc; \
+	done
 
 .PHONY: ccs
 
--- src/cc-layers-mat.cpp
+++ src/cc-layers-mat.cpp
@@ -11,12 +11,12 @@
 
 using namespace boost;
 
-typedef adjacency_list<vecS, vecS, undirectedS> graph_t;
+typedef adjacency_list<vecS, vecS, undirectedS, unsigned int> graph_t;
 typedef graph_traits<graph_t>::vertex_descriptor Vertex;
 typedef graph_traits<graph_t>::edge_descriptor Edge;
 
-graph_t g;
-
+std::unordered_map<unsigned int, graph_t> G;
+std::vector<unsigned int> ccs;
 /* long long currentTimeMilliS = 0; */
 
 /* long long currentTimeStamp() { */
@@ -37,33 +37,49 @@ graph_t g;
 /*	return timeElapsed; */
 /* } */
 
-void readGraph(const std::string &inputFile) {
+void readGraph(const std::string &inputFile, const std::string &ccfile) {
 	std::ifstream is;
 	is.open(inputFile);
-	unsigned int src, tgt;
+	unsigned int src, tgt, layer;
+	/* graph_t g; */
 	std::string line;
 	while (std::getline(is, line)) {
 		if (line[0] == '#')
 			continue;
 		std::replace(line.begin(), line.end(), ',', ' ');
-		/* std::cerr<<line; */
 		std::istringstream iss(line);
-		assert(iss >> src >> tgt);
-		assert(add_edge(src, tgt, g).second);
+		assert(iss >> src >> tgt >> layer);
+		/* if (G.count(layer) == 1) { */
+		/* 	g = G.find(layer)->second; */
+		/* } else { */
+		/* 	g = graph_t(); */
+		/* 	G[layer] = g; */
+		/* } */
+		assert(add_edge(src, tgt, G[layer]).second);
+	}
+	is.close();
+	is.open(ccfile);
+	unsigned int vert, cc;
+	ccs = std::vector<unsigned int>();
+	while (std::getline(is, line)) {
+		if (line[0] == '#')
+			continue;
+		std::replace(line.begin(), line.end(), ',', ' ');
+		std::istringstream iss(line);
+		assert(iss >> vert >> cc);
+		if (vert > ccs.size())
+			ccs.resize(vert);
+		ccs[vert] = cc;
 	}
 	is.close();
 }
 
- void writeToFile(const std::string &prefix, std::vector<unsigned int> components, unsigned int num_components) {
-	std::ofstream outputFile;
-	outputFile.open(prefix);
-	outputFile<<"# vertex,connected_component\n";
-	/* outputFile<<"# #components: "<<num_components<<"\n"; */
+ void writeToFile(std::ofstream &outputFile, unsigned int layer, std::vector<unsigned int> &components, unsigned int num_components) {
+	graph_t g = G[layer];
 	for(unsigned int i = 0; i < num_vertices(g); i++) {
 		if (degree(i, g) > 0)
-			outputFile<<i<<","<<components[i]<<"\n";
+			outputFile<<i<<","<<ccs[i]<<","<<layer<<","<<components[i]<<"\n";
 	}
-	outputFile.close();
 }
 
 /* void writeMetaData(std::string prefix, unsigned int NODENUM, unsigned int EDGENUM, unsigned int maxdeg, long long preprocessingTime, long long algorithmTime) { */
@@ -79,31 +95,34 @@ void readGraph(const std::string &inputFile) {
 /* } */
 
 int main(int argc, char *argv[]) {
-	if (argc < 4) {
-		std::cerr<<argv[0]<<": usage: ./connectedcomponents <path to graph> <layer> <path to layers dir>\n";
+	if (argc < 3) {
+		std::cerr<<argv[0]<<": usage: ./cc-layers-mat <path to graph> <path to connected components>\n";
 		exit(1);
 	}
 	std::string prefix = argv[1];
-	int layer = atol(argv[2]);
-	std::string prefixx;
-	if (layer <= 0) {
-		prefixx = prefix+".cc";
-	} else {
-		prefixx = argv[3];
-		prefixx += "/layer-"+std::to_string(layer)+".cc";
-	}
-
+	std::string prefixx = prefix.substr(0,prefix.length()-4)+".cc-layers";
+	std::string prefixcc = argv[2];
+	
+	G = std::unordered_map<unsigned int, graph_t>();
 	/* reset(); */
-	readGraph(prefix);
+	readGraph(prefix, prefixcc);
 	if(DEBUG)
-		std::cout<<"LOADED GRAPH "<<num_vertices(g)<<", "<<num_edges(g)<<"\n";
+		std::cout<<"LOADED GRAPH\n";
 	/* long long preprocessingTime = getTimeElapsed(); */
 	/* reset(); */
-
-	std::vector<unsigned int> components(num_vertices(g));
-	unsigned int num = connected_components(g, &components[0]);
-
+	
+	std::ofstream outputFile;
+	outputFile.open(prefixx);
+	outputFile<<"# vertex,connected_component,layer,connected_component_in_layer\n";
+	/* outputFile<<"# #components: "<<num_components<<"\n"; */
+	std::vector<unsigned int> components(10);
+	for (auto g = G.begin(); g != G.end(); g++) {
+		components.resize(num_vertices(g->second));
+		components.clear();
+		unsigned int num = connected_components(g->second, &components[0]);
+		writeToFile(outputFile, g->first, components, num);
+	}
+	outputFile.close();
 	/* long long algorithmTime = getTimeElapsed(); */
-	writeToFile(prefixx, components, num);
 	return 0;
 }
