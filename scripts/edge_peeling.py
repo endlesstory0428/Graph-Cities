b9c9f825b6f3be8eb719385577a06c97023f8a2a
Daniel Nakhimovich <dnahimov@gmail.com>
2020-03-09 03:05:23 -0400

    different inputs

--- scripts/edge_peeling.py
+++ scripts/edge_peeling.py
@@ -1,5 +1,5 @@
-import fileinput
-import pandas as pd
+import sys
+# import pandas as pd
 import networkx as nx
 
 MAX_EDGE = 3
@@ -17,17 +17,26 @@ def minEdge(G):
     return min(G.edges, key=lambda x: edeg(G, *x))
 
 
-g = nx.parse_edgelist((fileinput.input()), nodetype=int)
+delim = ' '
+if len(sys.argv) > 2:
+    delim = sys.argv[2]
+
+with open(sys.argv[1]) as f:
+    elist = [' '.join(x.strip().split(delim)[:2]) for x in f.readlines()]
+
+g = nx.parse_edgelist(elist, nodetype=int)
 
 peels = {}
 
-prev_deg = 1
-while g.number_of_edges() > 0:
-    e = minEdge(g)
-    deg = edeg(g, *e)
-    if deg > prev_deg:
-        prev_deg = deg
-    ean = set([tuple(sorted([e[0], x]))
-               for x in g[e[0]]]).union([tuple(sorted([e[1], x])) for x in g[e[1]]])
-    peels[prev_deg] = peels.get(prev_deg, []) + list(ean)
-    g.remove_edges_from(ean)
+
+def peel():
+    prev_deg = 1
+    while g.number_of_edges() > 0:
+        e = minEdge(g)
+        deg = edeg(g, *e)
+        if deg > prev_deg:
+            prev_deg = deg
+        ean = set([tuple(sorted([e[0], x]))
+                   for x in g[e[0]]]).union([tuple(sorted([e[1], x])) for x in g[e[1]]])
+        peels[prev_deg] = peels.get(prev_deg, []) + list(ean)
+        g.remove_edges_from(ean)
