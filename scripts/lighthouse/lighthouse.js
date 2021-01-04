e8eb9b4ab7996ff74455066a4a33353ed620a0a6
Laurel Han <chengguizi.han@gmail.com>
2021-01-04 12:48:21 -0500

    update sum with sum_log

--- scripts/lighthouse/lighthouse.js
+++ scripts/lighthouse/lighthouse.js
@@ -1,19 +1,21 @@
 import * as THREE from '../../node_modules/three/build/three.module.js';
-
+import {TrackballControls} from '../../node_modules/three/examples/jsm/controls/TrackballControls.js';
 let scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+let controls;
+let perspectiveCamera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 scene.background = new THREE.Color( 0xffffff );
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-camera.position.z = 1000;
-camera.position.y = 200;
+// const geometry = new THREE.BoxGeometry();
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+perspectiveCamera.position.z = 1000;
+perspectiveCamera.position.y = 200;
+createControls( perspectiveCamera );
 
 const data_list = ['com-friendster','movies','cit-Patents'];
 let manager = new THREE.LoadingManager();
@@ -24,16 +26,13 @@ manager.onStart = function(url,itemsLoaded,itemsTotal) {
 
 const animate = function () {
     requestAnimationFrame( animate );
-
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
-
-    renderer.render( scene, camera );
+    controls.update();
+    renderer.render( scene, perspectiveCamera );
 };
 
 animate();
 
-let result = createCitySummaryMesh(data_list[0], scene);
+let result = createCitySummaryMesh(data_list[2], scene);
 scene = result.scene;
 
 // create "lighthouse" mesh that summarize whole city information
@@ -52,11 +51,11 @@ function cylinderRadius(vh_h) {
     // return Math.sqrt(vh_h / Math.PI);
 }
 
-function sum(obj) {
+function sum_log(obj) {
     let sum = 0;
     for(let a in obj) {
         if(obj.hasOwnProperty(a)) {
-            sum += obj[a];
+            sum += Math.log2(obj[a]+1);
         }
     }
     return sum;
@@ -71,18 +70,24 @@ function loadCitySummaryFile(info, scene) {
     const peel_vals = Object.keys(info);
     const peel_value_range = Math.max(...peel_vals) - Math.min(...peel_vals);
     const peel_value_count = peel_vals.length;
+    console.log("peel_value_range",peel_value_range);
+    console.log("peel_value_count",peel_value_count);
     const peel_ratio = peel_value_range / peel_value_count;
 
     for(let key in info) {
-        original_height_sum = sum(info[key]);
-        // console.log("original_height_sum "+original_height_sum);
+        original_height_sum += sum_log(info[key]);
+        console.log("original_height_sum "+original_height_sum);
         const layer_vals = Object.values(info[key]);
         const layer_max_radius = Math.max(...layer_vals);
         // console.log(key+'/'+layer_max_radius);
         const rad = cylinderRadius(layer_max_radius);
         if(max_radius < rad){ max_radius = rad;}
     }
+
     scale_factor = peel_ratio * max_radius / original_height_sum;
+    console.log("max_height",original_height_sum);
+    console.log("max_radius",max_radius);
+    console.log("peel_ratio",peel_ratio);
     console.log("scale_factor", scale_factor);
 
     for(let key in info) {
@@ -98,10 +103,30 @@ function loadCitySummaryFile(info, scene) {
                 const cylinder = new THREE.Mesh(geometry,material);
                 scene.add(cylinder);
                 Y += Y_dis;
+                console.log("Y "+Y+" Y_dis "+Y_dis+" R "+R);
             }
         }
     }
     return {scene: scene};
+}
+
+function createControls( camera ) {
+    controls = new TrackballControls( camera, renderer.domElement );
+    controls.rotateSpeed = 1.0;
+    controls.zoomSpeed = 1.2;
+    controls.panSpeed = 0.8;
+    controls.keys = [ 65, 83, 68 ];
+}
+
+function onWindowResize() {
+    let aspect = window.innerWidth / window.innerHeight;
+
+    perspectiveCamera.aspect = aspect;
+    perspectiveCamera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    controls.handleResize();
 }
 
 export {createCitySummaryMesh};
