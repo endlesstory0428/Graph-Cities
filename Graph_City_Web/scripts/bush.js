51e2356941b55ebe72f2594110e908a7054a1443
endlesstory <603907639@qq.com>
2022-10-24 18:03:46 -0400

    update missing files

--- Graph_City_Web/scripts/bush.js
+++ Graph_City_Web/scripts/bush.js
@@ -1,4 +1,4 @@
-import * as THREE from '../node_modules/three/build/three.module.js';
+import * as THREE from '../lib/three/build/three.module.js';
 
 let bushes = {};
 let last = [-187.86920742571192,-69.84011743155536]
